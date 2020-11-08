a4c9f4ea60d5ee541387c95d1908d512c817e087
Yaron Cohen <yaroncbtv@gmail.com>
2020-11-08 18:52:26 +0200

    js pro Yaron Cohen v11.01 fix issues feedback v2

--- javaScript.js
+++ javaScript.js
@@ -9,32 +9,23 @@ function fibonacci(num) {
     num2 = sum;
   }
   return num2;
-
 }
-
 function hideElem() {
   document.getElementById("test").style.visibility = "hidden";
 }
-
 function showElem() {
   document.getElementById("test").style.visibility = "visible";
 }
-
-
 let arraySort = []
 fetch('http://localhost:5050/getFibonacciResults')
   .then(response =>
     response.json()
   )
   .then(data => {
-
     var cont = document.getElementById('result-2');
     var ul = document.createElement('ul');
     ul.setAttribute('style', 'padding: 0; margin: 0;');
     ul.setAttribute('id', 'theList');
-
-
-
     var e = "<hr/>";
     for (let i = 0; i < data.results.length; i++) {
       let mill = data.results[i].createdDate
@@ -42,22 +33,12 @@ fetch('http://localhost:5050/getFibonacciResults')
       dateConvvert = date.toString()
       var li = document.createElement('li');
       li.innerHTML += "The Fibonnaci Of " + data.results[i].number + " is " + data.results[i].result + ". " + "Calculated at: " + dateConvvert + "<hr/>";      // assigning text to li using array value.
-
       li.setAttribute('style', 'display: block;');
-
       ul.appendChild(li);
     }
-
     cont.appendChild(ul);
-
-
   });
-
-
 function onClickNumberAsc() {
-
-
-
   fetch('http://localhost:5050/getFibonacciResults')
     .then(response => response.json())
     .then(data => {
@@ -74,18 +55,12 @@ function onClickNumberAsc() {
           }
         }
       } while (swapped);
-
-
       var myobj = document.getElementById("theList");
       myobj.remove();
-
       var cont = document.getElementById('result-2');
       var ul = document.createElement('ul');
       ul.setAttribute('style', 'padding: 0; margin: 0;');
       ul.setAttribute('id', 'theList');
-
-
-
       var e = "<hr/>";
       for (let i = 0; i < data.results.length; i++) {
         let mill = data.results[i].createdDate
@@ -93,39 +68,17 @@ function onClickNumberAsc() {
         dateConvvert = date.toString()
         var li = document.createElement('li');
         li.innerHTML += "The Fibonnaci Of " + data.results[i].number + " is " + data.results[i].result + ". " + "Calculated at: " + dateConvvert + "<hr/>";      // assigning text to li using array value.
-
         li.setAttribute('style', 'display: block;');
-
         ul.appendChild(li);
       }
-
       cont.appendChild(ul);
-
-
-
-
-
-
     });
-
-
-
-
-
 }
-
 //---------------------------------------------------------------------------------------
-
-
 function onClickNumberDeac() {
-
-
-
   fetch('http://localhost:5050/getFibonacciResults')
     .then(response => response.json())
     .then(data => {
-
-
       for (let i = 0; i < (data.results.length - 1); i++) {
         for (let j = 0; j < data.results.length - i - 1; j++) {
           if (data.results[j].number < data.results[j + 1].number) {
@@ -135,17 +88,12 @@ function onClickNumberDeac() {
           }
         }
       }
-
       var myobj = document.getElementById("theList");
       myobj.remove();
-
       var cont = document.getElementById('result-2');
       var ul = document.createElement('ul');
       ul.setAttribute('style', 'padding: 0; margin: 0;');
       ul.setAttribute('id', 'theList');
-
-
-
       var e = "<hr/>";
       for (let i = 0; i < data.results.length; i++) {
         let mill = data.results[i].createdDate
@@ -153,31 +101,17 @@ function onClickNumberDeac() {
         dateConvvert = date.toString()
         var li = document.createElement('li');
         li.innerHTML += "The Fibonnaci Of " + data.results[i].number + " is " + data.results[i].result + ". " + "Calculated at: " + dateConvvert + "<hr/>";      // assigning text to li using array value.
-
         li.setAttribute('style', 'display: block;');
-
         ul.appendChild(li);
       }
-
       cont.appendChild(ul);
-
-
-
     });
-
-
 };
-
-
 //------------------------------------------------------------------------------
-
 function dateAsc() {
-
-
   fetch('http://localhost:5050/getFibonacciResults')
     .then(response => response.json())
     .then(data => {
-
       let swapped;
       do {
         swapped = false;
@@ -190,21 +124,12 @@ function dateAsc() {
           }
         }
       } while (swapped);
-
-
-
-
-
       var myobj = document.getElementById("theList");
       myobj.remove();
-
       var cont = document.getElementById('result-2');
       var ul = document.createElement('ul');
       ul.setAttribute('style', 'padding: 0; margin: 0;');
       ul.setAttribute('id', 'theList');
-
-
-
       var e = "<hr/>";
       for (let i = 0; i < data.results.length; i++) {
         let mill = data.results[i].createdDate
@@ -212,37 +137,16 @@ function dateAsc() {
         dateConvvert = date.toString()
         var li = document.createElement('li');
         li.innerHTML += "The Fibonnaci Of " + data.results[i].number + " is " + data.results[i].result + ". " + "Calculated at: " + dateConvvert + "<hr/>";      // assigning text to li using array value.
-
         li.setAttribute('style', 'display: block;');
-
         ul.appendChild(li);
       }
-
-      cont.appendChild(ul);
-
-
-
-
-
-
+      cont.appendChild(ul)
     });
-
-
-
-
 }
-
-
-
 function dateDeac() {
-
-
-
   fetch('http://localhost:5050/getFibonacciResults')
     .then(response => response.json())
     .then(data => {
-
-
       for (let i = 0; i < (data.results.length - 1); i++) {
         for (let j = 0; j < data.results.length - i - 1; j++) {
           if (data.results[j].createdDate < data.results[j + 1].createdDate) {
@@ -252,17 +156,12 @@ function dateDeac() {
           }
         }
       }
-
       var myobj = document.getElementById("theList");
       myobj.remove();
-
       var cont = document.getElementById('result-2');
       var ul = document.createElement('ul');
       ul.setAttribute('style', 'padding: 0; margin: 0;');
       ul.setAttribute('id', 'theList');
-
-
-
       var e = "<hr/>";
       for (let i = 0; i < data.results.length; i++) {
         let mill = data.results[i].createdDate
@@ -270,59 +169,35 @@ function dateDeac() {
         dateConvvert = date.toString()
         var li = document.createElement('li');
         li.innerHTML += "The Fibonnaci Of " + data.results[i].number + " is " + data.results[i].result + ". " + "Calculated at: " + dateConvvert + "<hr/>";      // assigning text to li using array value.
-
         li.setAttribute('style', 'display: block;');
-
         ul.appendChild(li);
       }
-
       cont.appendChild(ul);
-
-
-
     });
-
-
 };
-
-
 function onClickFibonacci() {
-
-
-
   var checkBox = document.getElementById("myCheck");
   if (checkBox.checked == true) {
-
-
     let numInput = document.getElementById("user-input").value;
-    // let numInput = '42';
     let URL = 'http://localhost:5050/fibonacci/'
     URL = URL + numInput
     let flag = false
     let result = document.getElementById("result");
-
     document.getElementById("user-input").style.color = "black";
     document.getElementById("user-input").style.borderColor = "#CCCCCC";
-
     result.innerHTML = '';
     result.style.color = "#373A3C"
     result.style.textDecoration = "underline"
     result.style.fontSize = "24px"
     result.style.fontWeight = "bold"
-
     targListAlert = document.getElementsByClassName("alert");
     if (targListAlert) {
       for (var x = 0; x < targListAlert.length; x++) {
         targListAlert[x].style.visibility = "hidden";
       }
     }
-
     result.innerHTML = ''
     result.style.visibility = "hidden"
-
-
-
-
     fetch(URL)
       .then((response) => {
         if (response.ok) {
@@ -346,7 +221,6 @@ function onClickFibonacci() {
                     targListSpin[x].style.visibility = "hidden";
                   }
                 }
-
                 result.style.visibility = "visible"
                 result.innerHTML = "Server Error: " + text;
                 result.style.color = "red"
@@ -354,11 +228,6 @@ function onClickFibonacci() {
                 result.style.fontSize = "15px"
                 result.style.fontWeight = "normal"
               }, 1500);
-
-
-
-
-
             } else {
               targListSpin = document.getElementsByClassName("spinner-border");
               if (targListSpin) {
@@ -380,24 +249,15 @@ function onClickFibonacci() {
                     targListAlert[x].innerHTML = text
                   }
                 }
-
                 document.getElementById("user-input").style.color = "#D9534F";
                 document.getElementById("user-input").style.borderColor = "#D9534F";
               }, 1500);
-
             }
-
-
-
           })
-
         }
       })
-
       .then((responseJson) => {
         if (flag === true) {
-
-
           targList = document.getElementsByClassName("spinner-border");
           if (targList) {
             for (var x = 0; x < targList.length; x++) {
@@ -411,38 +271,17 @@ function onClickFibonacci() {
                 targList[x].style.visibility = "hidden";
               }
             }
-
             document.getElementById("result").style.visibility = "visible";
             document.getElementById("result").innerHTML = responseJson.result;
-
-
-
-
-
-
             var contant = document.getElementById('result-2');
             let date = Date(responseJson.createdDate)
             contant.innerHTML += "NEW: The Fibonnaci Of " + responseJson.number + " is " + responseJson.result + ". " + "Calculated at: " + date + "<hr/>";      // assigning text to li using array value.
-
-
-
-
-
-
-
           }, 1500);
-
         }
       })
-
-
-
       .catch((error) => {
         console.log(error)
       });
-
-
-
   }
 
   //----------------------------------------------------------------------------------
@@ -471,9 +310,6 @@ function onClickFibonacci() {
           targList[x].style.visibility = "visible";
         }
       }
-
-
-
       setTimeout(function () {
         targList = document.getElementsByClassName("spinner-border");
         if (targList) {
@@ -491,18 +327,8 @@ function onClickFibonacci() {
           }
         }
       }, 1500);
-
-
-
-
-
-
-
-
     }
-
     else {
-
       targList = document.getElementsByClassName("alert");
       if (targList) {
         for (var x = 0; x < targList.length; x++) {
@@ -532,64 +358,5 @@ function onClickFibonacci() {
 
     }
   }
-
-
-
-
-
-
-
-  // fetch('http://localhost:5050/fibonacci/51')
-  //   .then(response => {
-  //     if(!response.ok) {
-
-  //       let input = '';
-  //       document.getElementById("result").innerHTML = input;
-
-  //       response.text().then(text => {
-
-  //         throw Error(text)
-
-
-  //       })
-
-  //     }
-  //     else {
-  //      return response.json();
-  //    }    
-  //   })
-
-  //   .catch(err => {
-  //     console.log('caught it!',err);
-  //   })
-
-
-  //   .then(data => {
-  //     console.log(data)
-
-  //   })
-
-
-
-
-
-
-
-
-
-
 }
 
-
-// ------------------------------------------------------------------------------------
-// let numInput = '51';
-  // let URL = 'http://localhost:5050/fibonacci/'
-  // URL = URL + numInput
-  // fetch(URL)
-
-  // .then(response => response.json())
-  // .then(data => 
-
-  //   console.log(data)
-
-  // )
